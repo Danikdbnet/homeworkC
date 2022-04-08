3c52801f6cf5adeb6ae93e37a2c416129682837d
Danikdbnet <95984132+Danikdbnet@users.noreply.github.com>
2022-04-08 19:00:38 +0200

    add towers of Hanoi & modified bubble sort

--- Library/myLIb.h
+++ Library/myLIb.h
@@ -1,17 +1,29 @@
  //Bubble Sort
- void bubble_sort (int *arr , int size , int i)
+ void bubble_sort (int *arr, int size, int i)
  {
-    for (int j=0 ; j<i ; j++)
+    for (int j = 0; j < i; j++)
     {
-        for(int k = 0 ; k < size-1 ; ++k)
+        for(int k = 0; k < size - 1 ; ++k)
         {
             if(arr[k] > arr[k+1])
             {
-                int temp = arr[k] ;
-                arr[k] = arr[k+1] ;
+                int temp = arr[k];
+                arr[k] = arr[k+1];
                 arr[k+1] = temp ;
             }
         }
     }
  }
 
+//Towers of Hanoi
+void hanoi(int n, char from, char to, char help)
+{
+    if (n == 1)
+    {
+        printf("presun disk 1 z %c na %c\n", from, to);
+        return;
+    }
+    hanoi(n-1, from, help, to);
+    printf("presun disk %d z %c na %c\n", n, from, to);
+    hanoi(n-1, help, to, from);
+}
